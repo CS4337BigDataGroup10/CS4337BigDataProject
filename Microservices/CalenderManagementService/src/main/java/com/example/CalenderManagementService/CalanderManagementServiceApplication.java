300d757af2141e31c7d8021be3a1c62b291a5bcd
BrenyQT <brendan.quinn2003@gmail.com>
2024-10-23 18:25:09 +0100

    trying to build all microservices using parent gradle file

---
+++ Microservices/CalenderManagementService/src/main/java/com/example/CalenderManagementService/CalanderManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.CalenderManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalanderManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalanderManagementServiceApplication.class, args);
+    }
+}
