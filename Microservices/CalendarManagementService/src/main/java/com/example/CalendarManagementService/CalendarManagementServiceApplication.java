ee0fec42d1d0b1012cc979e59b51ea936f64dc0a
BrenyQT <brendan.quinn2003@gmail.com>
2024-10-24 12:26:52 +0100

    builds all the gradle files

---
+++ Microservices/CalendarManagementService/src/main/java/com/example/CalendarManagementService/CalendarManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CalendarManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarManagementServiceApplication.class, args);
+	}
+
+}
---
+++ Microservices/CalendarManagementService/src/main/java/com/example/CalendarManagementService/entity/BookingMakingServiceEntity.java
@@ -0,0 +1,4 @@
+package com.example.CalendarManagementService.entity;
+
+public class BookingMakingServiceEntity {
+}
