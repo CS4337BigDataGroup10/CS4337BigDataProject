1def659c8144000b97c5a57855964d6f3663c8f2
eamonngorman <79876756+eamonngorman@users.noreply.github.com>
2024-11-27 22:21:04 +0000

    Delete Microservices/BookingMakingService/src/main/resources/Database/bookingserviceDB.sql
    Deleted to avoid merge conflict

--- Microservices/BookingMakingService/src/main/resources/Database/bookingserviceDB.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE bookings (
-    booking_id INT AUTO_INCREMENT PRIMARY KEY,
-    email_id VARCHAR(255) NOT NULL,
-    tour_id INT NOT NULL,
-    is_cancelled BOOLEAN NOT NULL DEFAULT FALSE
-);
