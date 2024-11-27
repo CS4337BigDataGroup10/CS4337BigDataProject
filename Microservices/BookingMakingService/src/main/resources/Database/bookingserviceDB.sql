3a959dff444cd4e0ab10a8c8265c37442337b922
eamonngorman <79876756+eamonngorman@users.noreply.github.com>
2024-11-27 14:37:06 +0000

    Updated DB structure to accomodate cancellation status

--- Microservices/BookingMakingService/src/main/resources/Database/bookingserviceDB.sql
+++ Microservices/BookingMakingService/src/main/resources/Database/bookingserviceDB.sql
@@ -1,5 +1,6 @@
 CREATE TABLE bookings (
     booking_id INT AUTO_INCREMENT PRIMARY KEY,
     email_id VARCHAR(255) NOT NULL,
-    tour_id INT NOT NULL
+    tour_id INT NOT NULL,
+    is_cancelled BOOLEAN NOT NULL DEFAULT FALSE
 );
