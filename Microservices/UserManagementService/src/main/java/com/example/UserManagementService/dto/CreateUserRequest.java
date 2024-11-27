d4c2469e4a9ca662028d1fb08a8fd86d3a3229b4
eamonngorman <79876756+eamonngorman@users.noreply.github.com>
2024-11-27 22:15:18 +0000

    UserManagementTestingNewBranch

--- Microservices/UserManagementService/src/main/java/com/example/UserManagementService/dto/CreateUserRequest.java
+++ Microservices/UserManagementService/src/main/java/com/example/UserManagementService/dto/CreateUserRequest.java
@@ -4,6 +4,12 @@ public class CreateUserRequest {
     private String givenName;
     private String familyName;
     private String email;
+    // Constructor
+    public CreateUserRequest(String givenName, String familyName, String email) {
+        this.givenName = givenName;
+        this.familyName = familyName;
+        this.email = email;
+    }
 
     // Getters and Setters
     public String getEmail() { return email; }
