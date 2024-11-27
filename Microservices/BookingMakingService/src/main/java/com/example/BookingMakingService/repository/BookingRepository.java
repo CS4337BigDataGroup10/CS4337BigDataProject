ac0ee95fe5ccbc198347a486a29f1623d336a58b
eamonngorman <79876756+eamonngorman@users.noreply.github.com>
2024-11-27 14:35:52 +0000

    Added new repository method for finding specific bookings by email

--- Microservices/BookingMakingService/src/main/java/com/example/BookingMakingService/repository/BookingRepository.java
+++ Microservices/BookingMakingService/src/main/java/com/example/BookingMakingService/repository/BookingRepository.java
@@ -3,8 +3,13 @@ package com.example.BookingMakingService.repository;
 import com.example.BookingMakingService.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
     // I am calling this method from BookingService. In booking service I am calling findAll() method.
     //This is a JpaRepository method that is used to fetch all the bookings from the database.
+
+    // Custom query method to find all bookings by emailId
+    List<Booking> findByEmailId(String emailId);
 }
