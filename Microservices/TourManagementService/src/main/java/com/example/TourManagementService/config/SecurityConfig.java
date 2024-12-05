6944b05a2b62b5cba1943fb1455dbbab368a7a93
BrenyQT <brendan.quinn2003@gmail.com>
2024-12-05 17:55:23 +0000

    Tour service JWT

---
+++ Microservices/TourManagementService/src/main/java/com/example/TourManagementService/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.TourManagementService.config;
+
+import com.example.TourManagementService.config.filter.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()) // Disable CSRF for APIs
+                .authorizeHttpRequests(authorize -> authorize
+                        // Require authentication for all endpoints
+                        .anyRequest().authenticated()
+                )
+                // Add the JwtFilter before the UsernamePasswordAuthenticationFilter
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
---
+++ Microservices/TourManagementService/src/main/java/com/example/TourManagementService/service/JwtService.java
@@ -0,0 +1,51 @@
+package com.example.TourManagementService.service;
+
+
+
+import com.example.TourManagementService.exceptions.JwtServiceExceptions;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final SecretKey JwtSecret;
+
+    public JwtService(@Value("${jwt.secret}") String googleClientSecret) {
+        this.JwtSecret = Keys.hmacShaKeyFor(googleClientSecret.getBytes());
+    }
+
+    public String generateToken(String email) {
+        // Define the token expiration time (1 hour in milliseconds)
+        long oneHourInMillis = 60 * 60 * 1000;
+
+        // Generate the token
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + oneHourInMillis))
+                .signWith(SignatureAlgorithm.HS256, JwtSecret)
+                .compact();
+    }
+
+    public Claims validateToken(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(JwtSecret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            throw new JwtServiceExceptions.TokenExpiredException("Token has expired", e);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new JwtServiceExceptions.InvalidTokenException("Token is invalid", e);
+        }
+    }
+
+
+}
+
