c0262fca924df4ab7cd5b682ef8d99737a6bef11
Kamil <kamilmrowiec02@gmail.com>
2024-12-06 17:51:31 +0000

    Fixed an exception that was causing the project to not build

--- Microservices/UserManagementService/src/main/java/com/example/UserManagementService/exceptions/UserManagementServiceExceptions.java
+++ Microservices/UserManagementService/src/main/java/com/example/UserManagementService/exceptions/UserManagementServiceExceptions.java
@@ -1,7 +1,7 @@
 package com.example.UserManagementService.exceptions;
 
 public class UserManagementServiceExceptions extends RuntimeException {
-    public UserManagementServiceExceptions(String message, Throwable cause) {
-        super(message, cause);
+    public UserManagementServiceExceptions(String message) {
+        super(message);
     }
 }
